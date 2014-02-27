5fb6ddd7ca8ba180f544bd61fb7ef54ff2c37677
jlevasseur <james.levasseur@umit.maine.edu>
2014-02-27 12:34:46 -0500

    Added a bunch of stuff, main menu, etc.

---
+++ UnityMaine/Assets/EvMsg.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvMsg : MonoBehaviour {
+
+	public GameObject[] containers;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
